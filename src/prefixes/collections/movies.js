6b281cea03ff263d4ba2819a343e1bdc2c4bf8ba
George Batt <george.batt@simpleviewinc.com>
2024-01-31 15:16:28 +0000

    Adds movie and people operations

--- src/prefixes/collections/movies.js
+++ src/prefixes/collections/movies.js
@@ -1,12 +1,67 @@
+const { query } = require("@simpleview/sv-graphql-client");
+
 class TrainingMovies {
     constructor({ graphUrl, graphServer }) {
         this._graphUrl = graphUrl;
         this._graphServer = graphServer;
     }
 
-    find({ fields, context, filter }) {}
-    insert({ fields, context, input }) {}
-    remove({ fields, context, filter }) {}
+    find({ fields, context, filter, headers }) {
+        return query({
+            query: `#graphql
+                query GetMovies($filter: training_movies_find_input) {
+                    training {
+                        movies_find(filter: $filter) {
+                            ${fields}
+                        }
+                    }
+                }
+            `,
+            variables: { filter },
+            url: this._graphUrl,
+            headers,
+            key: "training.movies_find",
+            clean: true,
+        });
+    }
+    insert({ fields, context, input, headers }) {
+        return query({
+            query: `#graphql
+                mutation InsertMovies($input: [training_movies_insert_input]!) {
+                    training {
+                        movies_insert(input: $input) {
+                            success
+                            message
+                        }
+                    }
+                }
+            `,
+            variables: { input },
+            url: this._graphUrl,
+            headers,
+            key: "training.movies_insert",
+            clean: true,
+        });
+    }
+    remove({ fields, context, filter, headers }) {
+        return query({
+            query: `#graphql
+                mutation RemoveMovies($filter: training_movies_remove_input) {
+                    training {
+                        movies_remove(filter: $filter) {
+                            success
+                            message
+                        }
+                    }
+                }
+            `,
+            variables: { filter },
+            url: this._graphUrl,
+            headers,
+            key: "training.movies_remove",
+            clean: true,
+        });
+    }
 }
 
 module.exports = TrainingMovies;
--- src/prefixes/collections/people.js
+++ src/prefixes/collections/people.js
@@ -1,12 +1,67 @@
+const { query } = require("@simpleview/sv-graphql-client");
+
 class TrainingPeople {
     constructor({ graphUrl, graphServer }) {
         this._graphUrl = graphUrl;
         this._graphServer = graphServer;
     }
 
-    find({ fields, context, filter }) {}
-    insert({ fields, context, input }) {}
-    remove({ fields, context, filter }) {}
+    find({ fields, context, filter, headers }) {
+        return query({
+            query: `#graphql
+                query GetPeople($filter: training_people_find_input) {
+                    training {
+                        people_find(filter: $filter) {
+                            ${fields}
+                        }
+                    }
+                }
+            `,
+            variables: { filter },
+            url: this._graphUrl,
+            headers,
+            key: "training.people_find",
+            clean: true,
+        });
+    }
+    insert({ fields, context, input, headers }) {
+        return query({
+            query: `#graphql
+                mutation InsertPeople($input: [training_people_insert_input]!) {
+                    training {
+                        people_insert(input: $input) {
+                            success
+                            message
+                        }
+                    }
+                }
+            `,
+            variables: { input },
+            url: this._graphUrl,
+            headers,
+            key: "training.people_insert",
+            clean: true,
+        });
+    }
+    remove({ fields, context, filter, headers }) {
+        return query({
+            query: `#graphql
+                mutation RemovePeople($filter: training_people_remove_input) {
+                    training {
+                        people_remove(filter: $filter) {
+                            success
+                            message
+                        }
+                    }
+                }
+            `,
+            variables: { filter },
+            url: this._graphUrl,
+            headers,
+            key: "training.people_remove",
+            clean: true,
+        });
+    }
 }
 
 module.exports = TrainingPeople;
